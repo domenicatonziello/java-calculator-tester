6d30d254ec32391cf6add47c5e425ade668dc082
domenicatonziello <domenica.tonziello10@gmail.com>
2023-05-23 15:35:22 +0200

    commit

--- src/main/java/org/java/Calculator.java
+++ src/main/java/org/java/Calculator.java
@@ -1,5 +1,29 @@
 package org.java;
 
 public class Calculator {
-
+	
+	public static float add(float num1, float num2) {
+		
+		return num1 + num2;
+	}
+	
+	public static float subtract(float num1, float num2) {
+		
+		return num1 - num2;
+	}
+	public static float divide(float num1, float num2) {
+		
+		if (num2 == 0) {
+			throw new ArithmeticException("Division by zero");
+		}
+		
+		return num1 / num2;
+	}
+	
+	public static float multiply(float num1, float num2) {
+		
+		return num1 * num2;
+	}
+	
+	
 }
--- src/test/java/org/java/test/CalculatorTest.java
+++ src/test/java/org/java/test/CalculatorTest.java
@@ -1,5 +1,78 @@
 package org.java.test;
 
-public class CalculatorTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
+import java.util.Random;
+
+import org.java.Calculator;
+import org.junit.jupiter.api.RepeatedTest;
+
+
+public class CalculatorTest {
+	
+	Random random = new Random();
+	
+	@RepeatedTest(10)
+	public void sumTest() {
+		final float num1 = random.nextFloat(0,100);
+		final float num2 = random.nextFloat(0,100);
+		
+		final float sum = num1 + num2;
+		
+		final float res = Calculator.add(num1,num2);
+		
+		assertEquals(sum, res);
+		
+	}
+	@RepeatedTest(10)
+	public void subtractTest() {
+		final float num1 = random.nextFloat(0,100);
+		final float num2 = random.nextFloat(0,100);
+		
+		final float sub = num1 - num2;
+		
+		final float res = Calculator.subtract(num1,num2);
+		
+		assertEquals(sub, res);
+		
+	}
+	@RepeatedTest(10)
+	public void multiplyTest() {
+		final float num1 = random.nextFloat(0,100);
+		final float num2 = random.nextFloat(0,100);
+		
+		final float multiply = num1 * num2;
+		
+		final float res = Calculator.multiply(num1,num2);
+		
+		assertEquals(multiply, res);
+		
+	}
+	@RepeatedTest(10)
+	public void divideTest()throws Exception {
+		final float num1 = random.nextFloat(0,100);
+		final float num2 = random.nextFloat(0,100);
+		
+		assumeTrue(num2 != 0);
+		
+		final float divide = num1 / num2;
+		
+		final float res = Calculator.divide(num1,num2);
+		
+		assertEquals(divide, res);
+		
+	}
+	public void divideExcTest() throws Exception {
+		
+		final float num1 = random.nextFloat(0,100);
+		final float num2 = random.nextFloat(0,100);
+		
+		assumeTrue(num2 == 0);
+		
+		assertThrows(Exception.class,
+						()->Calculator.divide(num1,num2));
+		
+	}
 }
